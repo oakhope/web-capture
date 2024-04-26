c18b251acc13f9a5a9e8c556d8d246c8bad5edd5
yqw <yq.w@qq.com>
2024-04-26 16:43:01 +0800

    fix the last frame error

--- demo/tmp2.c
+++ demo/tmp2.c
@@ -1,23 +1,20 @@
-AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVFrame *pFrameRGB, int videoStream, int ms) {
+AVFrame *readLastFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVFrame *pFrameRGB, int videoStream) {
     struct SwsContext *sws_ctx = NULL;
 
     AVPacket packet;
     AVFrame *pFrame = NULL;
+    AVFrame *lastFrame = NULL;
 
     pFrame = av_frame_alloc();
 
     sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
 
-    int timeStamp = ((double)ms / (double)1000) * pFormatCtx->streams[videoStream]->time_base.den / pFormatCtx->streams[videoStream]->time_base.num;
-
-    int ret = av_seek_frame(pFormatCtx, videoStream, timeStamp, AVSEEK_FLAG_BACKWARD);
+    int ret = av_seek_frame(pFormatCtx, videoStream, INT64_MAX, AVSEEK_FLAG_BACKWARD);
 
     if (ret < 0) {
         fprintf(stderr, "av_seek_frame failed\n");
         return NULL;
     }
-
-    int64_t target_pts = av_rescale_q(ms, (AVRational){1, 1000}, pFormatCtx->streams[videoStream]->time_base);
 
     while (av_read_frame(pFormatCtx, &packet) >= 0) {
         if (packet.stream_index == videoStream) {
@@ -28,22 +25,19 @@ AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVF
             }
 
             while (avcodec_receive_frame(pCodecCtx, pFrame) == 0) {
-                if (pFrame->pts >= target_pts) {
-                    sws_scale(sws_ctx, (uint8_t const *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
-
-                    sws_freeContext(sws_ctx);
-                    av_frame_free(&pFrame);
-                    av_packet_unref(&packet);
-
-                    return pFrameRGB;
+                if (lastFrame != NULL) {
+                    av_frame_free(&lastFrame);
                 }
+                lastFrame = av_frame_clone(pFrame);
             }
         }
     }
+
+    sws_scale(sws_ctx, (uint8_t const *const *)lastFrame->data, lastFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
 
     sws_freeContext(sws_ctx);
     av_frame_free(&pFrame);
     av_packet_unref(&packet);
 
-    return NULL;
+    return pFrameRGB;
 }
--- src/capture.c
+++ src/capture.c
@@ -78,6 +78,100 @@ AVFrame *initAVFrame(AVCodecContext *pCodecCtx, uint8_t **frameBuffer) {
 //    return NULL;
 //}
 
+//AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVFrame *pFrameRGB, int videoStream, int ms) {
+//    struct SwsContext *sws_ctx = NULL;
+//
+//    AVPacket packet;
+//    AVFrame *pFrame = NULL;
+//
+//    pFrame = av_frame_alloc();
+//
+//    sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+//
+//    int timeStamp = ((double)ms / (double)1000) * pFormatCtx->streams[videoStream]->time_base.den / pFormatCtx->streams[videoStream]->time_base.num;
+//
+//    int ret = av_seek_frame(pFormatCtx, videoStream, timeStamp, AVSEEK_FLAG_BACKWARD);
+//
+//    if (ret < 0) {
+//        fprintf(stderr, "av_seek_frame failed\n");
+//        return NULL;
+//    }
+//
+//    int64_t target_pts = av_rescale_q(ms, (AVRational){1, 1000}, pFormatCtx->streams[videoStream]->time_base);
+//
+//    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+//        if (packet.stream_index == videoStream) {
+//            if (avcodec_send_packet(pCodecCtx, &packet) != 0) {
+//                fprintf(stderr, "avcodec_send_packet failed\n");
+//                av_packet_unref(&packet);
+//                continue;
+//            }
+//
+//            while (avcodec_receive_frame(pCodecCtx, pFrame) == 0) {
+//                if (pFrame->pts >= target_pts) {
+//                    sws_scale(sws_ctx, (uint8_t const *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
+//
+//                    sws_freeContext(sws_ctx);
+//                    av_frame_free(&pFrame);
+//                    av_packet_unref(&packet);
+//
+//                    return pFrameRGB;
+//                }
+//            }
+//        }
+//    }
+//
+//    sws_freeContext(sws_ctx);
+//    av_frame_free(&pFrame);
+//    av_packet_unref(&packet);
+//
+//    return NULL;
+//}
+
+AVFrame *readLastFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVFrame *pFrameRGB, int videoStream) {
+    struct SwsContext *sws_ctx = NULL;
+
+    AVPacket packet;
+    AVFrame *pFrame = NULL;
+    AVFrame *lastFrame = NULL;
+
+    pFrame = av_frame_alloc();
+
+    sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+
+    int ret = av_seek_frame(pFormatCtx, videoStream, INT64_MAX, AVSEEK_FLAG_BACKWARD);
+
+    if (ret < 0) {
+        fprintf(stderr, "av_seek_frame failed\n");
+        return NULL;
+    }
+
+    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+        if (packet.stream_index == videoStream) {
+            if (avcodec_send_packet(pCodecCtx, &packet) != 0) {
+                fprintf(stderr, "avcodec_send_packet failed\n");
+                av_packet_unref(&packet);
+                continue;
+            }
+
+            while (avcodec_receive_frame(pCodecCtx, pFrame) == 0) {
+                if (lastFrame != NULL) {
+                    av_frame_free(&lastFrame);
+                }
+                lastFrame = av_frame_clone(pFrame);
+            }
+        }
+    }
+
+    sws_scale(sws_ctx, (uint8_t const *const *)lastFrame->data, lastFrame->linesize, 0, pCodecCtx->height, pFrameRGB->data, pFrameRGB->linesize);
+
+    sws_freeContext(sws_ctx);
+    av_frame_free(&pFrame);
+    av_packet_unref(&packet);
+
+    return pFrameRGB;
+}
+
 AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVFrame *pFrameRGB, int videoStream, int ms) {
     struct SwsContext *sws_ctx = NULL;
 
@@ -94,7 +188,7 @@ AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVF
 
     if (ret < 0) {
         fprintf(stderr, "av_seek_frame failed\n");
-        return NULL;
+        return readLastFrame(pCodecCtx, pFormatCtx, pFrameRGB, videoStream);
     }
 
     int64_t target_pts = av_rescale_q(ms, (AVRational){1, 1000}, pFormatCtx->streams[videoStream]->time_base);
@@ -125,7 +219,7 @@ AVFrame *readAVFrame(AVCodecContext *pCodecCtx, AVFormatContext *pFormatCtx, AVF
     av_frame_free(&pFrame);
     av_packet_unref(&packet);
 
-    return NULL;
+    return readLastFrame(pCodecCtx, pFormatCtx, pFrameRGB, videoStream);
 }
 
 // 读取帧数据并返回 uint8 buffer
